a5f4cede7dc1a6621fa0f796ab1adeda918083f3
RAKS Lakshani <140134810+RAKSLAKSHANI@users.noreply.github.com>
2025-03-07 10:06:28 +0530

    first commit

---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+      we are in profile 
+    </div>
+  )
+}
+
+export default ProfileUpdate
