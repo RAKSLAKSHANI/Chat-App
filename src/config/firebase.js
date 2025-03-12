f7ad60ca1a75507e9bee657957ad91fea43dafc4
RAKS Lakshani <140134810+RAKSLAKSHANI@users.noreply.github.com>
2025-03-12 23:32:43 +0530

    setting up authentication
    Adding logic to display selected image on the profile update page and  Setting up authentication, Firestore database, and storage for the chat application

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzcOZ4mlk-8kmXFH0OZcnobSho_83tN6A",
+  authDomain: "chat-app-d991c.firebaseapp.com",
+  projectId: "chat-app-d991c",
+  storageBucket: "chat-app-d991c.firebasestorage.app",
+  messagingSenderId: "251279750702",
+  appId: "1:251279750702:web:62fef5cdbfe83f52e691bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
